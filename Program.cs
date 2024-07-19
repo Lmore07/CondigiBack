a68f92fe68b6f0fa705fad585a24dd22e03916f2
Lmore07 <lmoreirat@uteq.edu.ec>
2024-07-19 00:16:50 -0500

    cambios en el program.cs

--- Program.cs
+++ Program.cs
@@ -103,7 +103,7 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
         var errorResponse = new BadRequestResponse<object>
         {
             StatusCode = (int)HttpStatusCode.BadRequest,
-            Error = "Se han encontrado errores de validaci�n.",
+            Error = "Se han encontrado errores de validación.",
             Errors = errors
         };
         return new BadRequestObjectResult(errorResponse);
